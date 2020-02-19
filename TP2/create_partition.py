409b23f1e8845aa53ec776cd86aa7c70606cacbf
Antoine Gaulin <antoine.gaulin@polymtl.com>
2020-02-19 11:22:12 -0500

    fix

--- TP2/create_partition.py
+++ TP2/create_partition.py
@@ -1,21 +1,18 @@
 import numpy as np
 import os
 
-
 def create_partition():
-    tailles_exemplaires = [1000, 3000, 10000, 30000,
-                           100000, 300000, 1000000, 3000000, 6000000, 8000000]
+    tailles_exemplaires = [1000, 3000, 10000, 30000, 100000, 300000, 1000000, 3000000, 6000000, 8000000]
     try:
         os.mkdir("exemplaires")
         for nb_notes in tailles_exemplaires:
-            for i in range(1, 11):
-                with open(os.path.join("exemplaires", f"ex_{nb_notes}_{i}.txt"), 'w') as f:
-                    print(str(nb_notes), file=f)
-                    notes = np.random.randint(
-                        0, 24, size=nb_notes, dtype='int64')
-                    print(' '.join([str(n) for n in notes]), file=f)
+            for i in range(1, 11):              
+                with open(os.path.join("exemplaires", "ex_" + str(nb_notes) + "_" + str(i) + ".txt"), 'w') as fichier_exemplaire:
+                    fichier_exemplaire.write(str(nb_notes) + "\n")
+                    notes = np.random.randint(0, 24, size=nb_notes, dtype='int64')
+                    fichier_exemplaire.write(' '.join([str(n) for n in notes]))
     except:
-        print("Les exemplaires ont déjà été créés. Si vous voulez les créer à nouveau, supprimer le dossier 'exemplaires'.")
+        print("Les exemplaires ont deja ete crees. Si vous voulez les creer a nouveau, supprimer le dossier exemplaires.")
 
 
 if __name__ == '__main__':
