394f0a03bf37844d8229840b4402f565ced82b96
Antoine Gaulin <antoine.gaulin@polymtl.com>
2020-03-11 11:52:58 -0400

    fini dyn et nei

--- TP2/main.py
+++ TP2/main.py
@@ -1,6 +1,6 @@
-import math, sys, time
+import math, sys, time, copy
 import numpy as np
-import copy
+from random import randint
 
 def notes_load(path):
     f = open(path, "r")
@@ -25,7 +25,55 @@ def glouton(size, notes, cout):
         s.append((d2, c[notes[i], d1, notes[i + 1], d2]))
         d1 = d2
 
-    return s
+    return map(list, zip(*s))
+
+def dynamic_prog(size, notes, cout):
+    c = np.zeros((5, size), dtype=int)
+    d = np.zeros((5, size), dtype=int)
+
+    for i in range(size - 2, -1, -1):
+        for j in range(5):
+            tmp = cout[notes[i], j, notes[i + 1], :] + c[:, i + 1]
+            c[j][i] = np.min(tmp)
+            d[j][i] = np.argmin(tmp)
+
+    doigts = []
+    curr_idx = np.argmin(c[:, 0])
+    doigts.append(curr_idx)
+
+    cout_transition = c[curr_idx, 0]
+
+    for i in range(size - 1):
+        curr_idx = d[curr_idx, i]
+        doigts.append(curr_idx)
+
+    return doigts, cout_transition
+
+def neighbors(size, notes, cout):
+    d, c = glouton(size, notes, cout)
+
+    best_d = d
+    best_c = c
+
+    n_iterations = 100000
+    for i in range (n_iterations):
+        d_curr = copy.deepcopy(d)
+        c_curr = copy.deepcopy(c)
+
+        rand_d = randint(0, 4)
+        rand_n = randint(0, size - 2)
+
+        d_curr[rand_n] = rand_d
+        c_curr[rand_n] = cout[notes[rand_n], d[rand_n], notes[rand_n + 1], rand_d]
+
+        if np.sum(c_curr) < np.sum(best_c):
+            best_d = d_curr
+            best_c = c_curr
+
+        d = d_curr
+        c = c_curr
+
+    return best_d, best_c
 
 if __name__=="__main__":
 
@@ -41,19 +89,19 @@ if __name__=="__main__":
 
     begin = time.time()
     if algo == "glouton":
-        s = glouton(size, notes, cout_transition)
+        d, c = glouton(size, notes, cout_transition)
+        c = np.sum(c)
     elif algo == "dp":
-        pass
+        d, c = dynamic_prog(size, notes, cout_transition)
     else:
-        pass
+        d, c = neighbors(size, notes, cout_transition)
+        c = np.sum(c)
 
     if sys.argv.count("-t"):
         print((time.time() - begin) * 1000)
 
-    d, c = map(list, zip(*s))
-
     if sys.argv.count("-p"):
-        print(np.sum(c))
+        print(c)
 
     if sys.argv.count("-c"):
         print(*d)
